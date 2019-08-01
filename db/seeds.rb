90325bac22c3c56cea80e6dc8209a3649c099b0e
Gopi Patel <ask2gopi@gmail.com>
2019-08-01 13:37:07 +1000

    new seed data added

--- db/seeds.rb
+++ db/seeds.rb
@@ -26,26 +26,40 @@ u5 = User.create :name => 'Paul', :email => 'paul@gmail.com', :password => 'chic
 Meeting.destroy_all
 puts "Creating Meetings"
 
-m1 = Meeting.create :title => 'Monday meeting', :agenda1 => 'New Busuiness', :agenda2 => 'planning', :duration =>30, :start => '2019-08-10'
-m2 = Meeting.create :title => 'Wednesday meeting', :agenda1 =>'openday planning', :duration => 30, :start => '2019-08-11'
-m3 = Meeting.create :title => 'Friday meeting', :agenda1 => 'graduation planning', :duration => 60, :start => '2019-07-31'
+m1 = Meeting.create :title => 'client Requirement changed',:description =>'Client has made some changes to the requirement we need to do this chenges as a high priority', :agenda1 => 'UI Changes', :agenda2 => 'database changes',:agenda3=>'Discuss on time it will take',:duration => 45, :start => '2019-08-10'
+
+m2 = Meeting.create :title => 'open day planning', :description => 'what are we doing on the dayand how all staff and voluntaries are involved', :agenda1 =>'staff responsibility',:agenda2 => 'voluntaries', :agenda3 => 'setup on the day' ,:duration => 30, :start => '2019-08-11'
+
+m3 = Meeting.create :title => 'Agile Retro', :description => 'Feedback about the project how was the project release', :agenda1 => 'What went well',:agenda2 =>'what to improve', :agenda3 => 'what went wrong', :duration => 60, :start => '2019-08-11'
 
 
 Action.destroy_all
 puts "Creating Actions"
 
-a1 = Action.create :description => 'My todo 1', :is_done => false
-a2 = Action.create :description => 'My todo 2', :is_done => false
-a3 = Action.create :description => 'My todo 3', :is_done => true
-a4 = Action.create :description => 'My todo 4', :is_done => true
+a1 = Action.create :description => 'Change the fonts and the home page to carousel theme',:duedate => '2019-09-10', :is_done => false
+a2 = Action.create :description => 'Have to add upload image field in the user database.',:duedate => '2019-09-01', :is_done => false
+a3 = Action.create :description =>'Neet to test all the changes',:duedate => '2019-09-10', :is_done => false
+
+a4 = Action.create :description =>'Have to assign the task to all staff',:duedate => '2019-09-10', :is_done => true
+a5 = Action.create :description =>'talk to studnets who are will to work on open day',:duedate => '2019-09-10', :is_done => true
+a6 = Action.create :description =>'come early to help setup',:duedate => '2019-09-10', :is_done => false
+
+
+a7 = Action.create :description =>'did not get the work done front end team',:duedate => '2019-09-01', :is_done => true
+a8 = Action.create :description =>'client was clear on his side on what he needs',:duedate => '2019-09-02', :is_done => true
+a9 = Action.create :description =>'story was not clear at the start of the project',:duedate => '2019-09-04', :is_done => true
 
 
 # Meeting assigned to User
-u1.hostedMeetings << m1 << m2 << m3
+u1.hostedMeetings << m1
+u2.hostedMeetings << m2
+u3.hostedMeetings << m3
 u1.meetings << m1 << m2 << m3
-
 u2.meetings << m1 << m2
-u3.meetings << m2
+u3.meetings << m2 << m3 << m1
+u4.meetings << m2 << m3
+u5.meetings << m2 << m3
+
 
 
 # Actions assigned to User and Meeting
